b361fa7ff3d269d1cf52af043d31292c9a77f615
Danilo Pianini <danilo.pianini@unibo.it>
2017-11-15 16:36:39 +0100

    Update encrypted iv

--- travis_build.sh
+++ travis_build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-openssl aes-256-cbc -K $encrypted_0f1727a4905b_key -iv $encrypted_0f1727a4905b_iv -in prepare_environment.sh.enc -out prepare_environment.sh -d
+openssl aes-256-cbc -K $encrypted_c97e58f83f35_key -iv $encrypted_c97e58f83f35_iv -in prepare_environment.sh.enc -out prepare_environment.sh -d
 bash prepare_environment.sh
 ./gradlew
 ./gradlew uploadArchives
